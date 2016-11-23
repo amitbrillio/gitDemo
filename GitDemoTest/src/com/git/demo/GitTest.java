c0fc809ed0e09ab21e1d81e2aee6b2ba90851ed3
Amit <Amit@LAPTOP-AC3SP1OG>
2016-11-23 22:51:51 +0530

    Demo commmit

---
+++ GitDemoTest/src/com/git/demo/GitTest.java
@@ -0,0 +1,16 @@
+package com.git.demo;
+
+public class GitTest {
+
+	public GitTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("learn git");
+		
+	
+	}
+
+}
